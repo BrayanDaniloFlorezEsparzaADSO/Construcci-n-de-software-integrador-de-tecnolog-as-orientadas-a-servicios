e960e96612f38d6bd4fefddce07c152b10e708e9
BrayanDaniloFlorezEsparzaADSO <brayandanilo2004@gmail.com>
2022-11-14 18:55:56 -0500

    2

--- main - copia.js
+++ main - copia.js
@@ -5,45 +5,17 @@ addEventListener("DOMContentLoaded", (e)=>{
             e.preventDefault();       
     
             let datosEntrada = Object.fromEntries(new FormData(e.target))
-                let dolares= Number (datosEntrada.dolares)
-                let pesos= 4809.50
-                let res= dolares * pesos
                 
-                
-                
+                let nciudad= datosEntrada.ciudad
+                let f= Number(datosEntrada.Fahrenheit)
+                let res=(f-32) * 5/9
                 let tabla=document.querySelector("tbody");
                 tabla.insertAdjacentHTML("beforeend",`
                     <tr>
-                        <td> ${dolares}  </td>
+                        <td> ${nciudad}  </td>
+                        <td> ${f} </td>
                         <td> ${res} </td>
                     </tr>
                     `)
-        })         
-        let form2=document.querySelector('#input2')
-         
-        form2.addEventListener("submit",(e)=>{
-            e.preventDefault();       
-    
-            let datosEntrada2 = Object.fromEntries(new FormData(e.target))
-                
-                
-                
-                
-                
-                let pesos2= Number (datosEntrada2.pesos)
-                let dolares2= 0.00021
-                let res2= dolares2 * pesos2
-                
-                
-                
-                let tabla2=document.querySelector("tbody[name=tbody]");
-                tabla2.insertAdjacentHTML("beforeend",`
-                    <tr>
-                        <td> ${pesos2}  </td>
-                        <td> ${res2} </td>
-                    </tr>
-                    `)
-                
-                
-        })    
+        })            
     })
