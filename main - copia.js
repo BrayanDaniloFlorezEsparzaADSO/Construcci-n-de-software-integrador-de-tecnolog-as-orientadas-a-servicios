34cf84b6b66229b292666e0b8d959001a0330bb3
BrayanDaniloFlorezEsparzaADSO <brayandanilo2004@gmail.com>
2022-11-14 19:38:32 -0500

    3

--- main - copia.js
+++ main - copia.js
@@ -1,21 +1,23 @@
 addEventListener("DOMContentLoaded", (e)=>{
         let form=document.querySelector('#input')
-    
+        let cont =1   
         form.addEventListener("submit",(e)=>{
             e.preventDefault();       
-    
+            if(cont <=50){
             let datosEntrada = Object.fromEntries(new FormData(e.target))
                 
-                let nciudad= datosEntrada.ciudad
-                let f= Number(datosEntrada.Fahrenheit)
-                let res=(f-32) * 5/9
+                let observaciones= datosEntrada.observaciones
+                let hora= (datosEntrada.hora)
+            
                 let tabla=document.querySelector("tbody");
                 tabla.insertAdjacentHTML("beforeend",`
                     <tr>
-                        <td> ${nciudad}  </td>
-                        <td> ${f} </td>
-                        <td> ${res} </td>
+                        <td> ${cont}  </td>
+                        <td> ${observaciones}  </td>
+                        <td> ${hora} </td>
                     </tr>
                     `)
-        })            
+                cont++
+            }
+        })               
     })
